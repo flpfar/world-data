4a31e9fb75b8635c4610d6224e9723961d6de057
Felipe Rosa <flp.far@gmail.com>
2020-08-17 19:40:30 -0300

    Add styles to Page404

--- src/pages/Page404/index.js
+++ src/pages/Page404/index.js
@@ -1,9 +1,15 @@
 import React from 'react';
+import Navbar from '../../components/Navbar';
+import styles from './styles.module.css';
 
-const Page404 = () => {
-  return (
-    <p>Page Not Found</p>
-  );
-};
+const Page404 = () => (
+  <>
+    <Navbar back />
+    <div className={styles.PageContainer}>
+      <h1>404</h1>
+      <p>Page not found</p>
+    </div>
+  </>
+);
 
 export default Page404;
